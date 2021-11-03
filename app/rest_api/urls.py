ac130f257e52c0b81df77b911b100886e2f09e73
Eugene Suhanov <suhanoves@gmail.com>
2021-11-03 18:28:42 +0300

    add endpoint 'report/'

--- app/rest_api/urls.py
+++ app/rest_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from rest_api.views import PollViewSet, AnswerViewSet
+from rest_api.views import PollViewSet, ReportView
 
 router = DefaultRouter()
 router.register(r'polls', PollViewSet)
-router.register(r'answers', AnswerViewSet)
 
 urlpatterns = [
+    path('report/', ReportView.as_view()),
     path('', include(router.urls)),
 ]
