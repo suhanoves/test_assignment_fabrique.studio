0d86abaadfe7c00ec791304f8b0d404c23e6b4f8
Eugene Suhanov <suhanoves@gmail.com>
2021-11-03 12:50:59 +0300

    add views for poll list and poll detail

--- app/rest_api/views.py
+++ app/rest_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import viewsets
 
-# Create your views here.
+from polls.models import Poll
+from rest_api.serializers import PollListSerializer, PollDetailSerializer
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    today = timezone.localtime(timezone.now()).date()
+
+    queryset = Poll.objects.filter(pub_date__lte=today, expiry_date__gte=today)
+    serializer_class = PollDetailSerializer
+
+    def get_serializer_class(self):
+        if self.action in ('list',):
+            return PollListSerializer
+        return super().get_serializer_class()  # for create/destroy/update
